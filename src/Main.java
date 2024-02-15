95e0c4ae356b4a25e2bc0ae4762e68cc7c515375
samet.uyanik <samet.uyanik2756@gmail.com>
2024-02-15 15:40:46 +0300

    Interface created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+        *
+        * Interface kullanımı
+        * Şirketin kendi çalışanını ve şirketin anlaşmalı olduğu yerin çalışanlarını baz aldım.
+        * Interface'ler oluşturarak kendi çalışanlarımızın ve dış kaynak çalışanlarının maaş,yemek,iş konularını gösterdim.
+        *
+        * */
+
+
     }
 }
---
+++ src/OutsourceWorker.java
@@ -0,0 +1,7 @@
+public class OutsourceWorker implements IWorkable{
+    //Dış kaynak çalışanı olduğu için yemek,maas konusu bizi ilgilendirmediği için implements almıyor.
+    @Override
+    public void work() {
+
+    }
+}
